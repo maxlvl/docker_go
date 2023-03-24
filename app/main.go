0cfb1177fa17e1d754a4c5c2c70bfd4c863eef2c
mx <max@maxvl.com>
2023-03-23 23:55:27 -0400

    Pass underlying command exit code to parent exit code

--- app/main.go
+++ app/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
@@ -17,7 +15,9 @@ func main() {
 
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
+    if exitError, ok := err.(*exec.ExitError); ok {
+      exitCode := exitError.ProcessState.ExitCode()
+      os.Exit(exitCode)
+    }
 	}
 }
