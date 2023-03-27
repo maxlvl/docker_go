b305832e967c337c9b45fee4cf2dd3b804c99bc5
mx <max@maxvl.com>
2023-03-26 23:07:07 -0400

    Fixed issue by running sys commands directly (vs trying to write custom copy logic) and handle null stdin, stdout and stderr

--- app/main.go
+++ app/main.go
@@ -1,94 +1,56 @@
 package main
 
 import (
-  "io"
-	"os"
+  "os"
 	"os/exec"
-  "path/filepath"
   "syscall"
+  "io/ioutil"
   "fmt"
+  "log"
+  "path"
 )
 
-func copyFile(src, dst string) error {
-  fmt.Println(src)
 
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-
-	dstFile, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer dstFile.Close()
-
-	_, err = io.Copy(dstFile, srcFile)
-	if err != nil {
-		return err
-	}
-
-	return dstFile.Sync()
-}
-
-type nullReader struct{}
-type nullWriter struct{}
-func (nullReader) Read(p []byte) (n int, err error)  { return len(p), nil }
-func (nullWriter) Write(p []byte) (n int, err error) { return len(p), nil }
 
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
-	cmd := exec.Command(command, args...)
 
-  dir := "chroot_dir"
-  err := os.Mkdir(dir, 0755)
+  tmpDir, err := ioutil.TempDir("", "")
   if err != nil {
-    fmt.Println("oops!")
-    return
+    log.Fatal("tmpdir: ", err)
   }
 
-  err = syscall.Chroot(dir)
+  commandPath := path.Join(tmpDir, command)
+
+  if err := exec.Command("mkdir", "-p", path.Dir(commandPath)).Run(); err != nil {
+    log.Fatal("mkdir: ", err)
+  }
+  
+
+  if err := exec.Command("cp", "-f", command, commandPath).Run(); err != nil {
+    log.Fatal("copy: ", err)
+  }
+
+
+  err = syscall.Chroot(tmpDir)
   if err != nil {
     fmt.Println("oopser!")
     return 
   }
-
-
-  dockerExplorerPath := "/usr/local/bin/docker-explorer"
-  dockerExplorerDestinationPath := filepath.Join(dir, "docker-explorer")
-
-  err = copyFile(dockerExplorerPath, dockerExplorerDestinationPath)
-	if err != nil {
- 	  fmt.Println("Error copying docker-explorer path:", err)
-	 	return
-	}
-
-	commandPath, err := exec.LookPath(command)
-	if err != nil {
-		fmt.Println("Error finding command path:", err)
-		return
-	}
-
-  fmt.Println(commandPath)
-	dstCommandPath := filepath.Join(dir, filepath.Base(commandPath))
-	err = copyFile(commandPath, dstCommandPath)
-	if err != nil {
-		fmt.Println("Error copying command:", err)
-		return
-	}
 
   err = os.Chdir("/")
   if err != nil {
     fmt.Println("oopsest!")
     return 
   } 
-  cmd.Stdin = nullReader{}
- 	cmd.Stdout = nullWriter{}
- 	cmd.Stderr = nullWriter{}
 
-  cmd.Path = dstCommandPath
+	cmd := exec.Command(command, args...)
+
+  cmd.Stdin = os.Stdin
+ 	cmd.Stdout = os.Stdout
+ 	cmd.Stderr = os.Stderr
+
 	err = cmd.Run()
 	if err != nil {
     fmt.Println(err)
